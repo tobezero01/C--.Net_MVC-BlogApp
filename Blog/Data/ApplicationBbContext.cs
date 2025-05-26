e31d3b000a32b7c6df3a990613cd54bea7270d4f
tobezero01 <ducnhuad@gmail.com>
2025-05-26 16:12:07 +0700

    setup EF core

---
+++ Blog/Data/ApplicationBbContext.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+	public class ApplicationBbContext : DbContext
+	{
+		public ApplicationBbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<BlogPost> BlogPosts { get; set; }
+		public DbSet<Tag> Tags { get; set; }
+
+	}
+}
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,9 +1,19 @@
+using Blog.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<ApplicationBbContext>(opt =>
+{
+	opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
